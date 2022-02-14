4ef16661eeeb3420dc2bffab078eb5dfd7047d9c
Satyasri Kar <Satyasri.Kar@axisbank.com>
2022-02-15 01:20:28 +0530

    Mouse press/release event attached to Grid2 Canvas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Grid1 from "./Grid1";
 import Home from "./Home";
 import Grid2 from "./Grid2";
+import Art1 from "./Art1";
 const App = () => {
   return (
     <Router>
@@ -14,6 +15,7 @@ const App = () => {
             <Route exact path="/noise1" element={<Noise1 />}></Route>
             <Route exact path="/grid1" element={<Grid1 />}></Route>
             <Route exact path="/grid2" element={<Grid2 />}></Route>
+            <Route exact path="/art1" element={<Art1 />}></Route>
           </Routes>
         </div>
       </div>
